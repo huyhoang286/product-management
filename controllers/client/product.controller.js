5c6605cd53096266a3dab30185d9f487bb0f3425
huyhoang286 <huyhoang.m28@gmail.com>
2026-02-20 14:56:57 +0700

    Xây dựng trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,20 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    })
+    const newProducts = products.map(item => {
+        item.newPrice = item.price*(1-item.discountPercentage/100).toFixed(2)
+        return item
+    })
+
+    console.log(newProducts)
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sản phẩm"
+        pageTitle: "Trang danh sách sản phẩm",
+        products: newProducts
     })
 }
