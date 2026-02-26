5e4975b676baa61fd3e4f63810b6976f6bf821ce
huyhoang286 <huyhoang.m28@gmail.com>
2026-02-26 16:55:25 +0700

    Xây dựng tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,31 +2,50 @@
 const Product = require("../../models/product.model")
 const filterStatusHepler = require("../../helpers/filterStatus")
 const searchHepler = require("../../helpers/search")
+const paginationHepler = require("../../helpers/pagination")
 
 module.exports.index = async (req, res) => {
-    const filterStatus = filterStatusHepler(req.query) 
+    const filterStatus = filterStatusHepler(req.query)
 
     let find = {
         deleted: false
     }
-    if(req.query.status) {
+    if (req.query.status) {
         find.status = req.query.status
     }
-    //Code Tìm kiếm
+
+    //Search
     const objectSearch = searchHepler(req.query)
-    console.log(objectSearch)
-    if(objectSearch.keyword) {
-        find.title = objectSearch.regex 
+    if (objectSearch.keyword) {
+        find.title = objectSearch.regex
     }
+    //End Search
+
+    //Pagination
+    const countProducts = await Product.countDocuments(find)
+    let objectPagination = paginationHepler(
+        {
+        currentPage: 1,
+        limitItems: 5
+        },
+        req.query,
+        countProducts
+    )
+    //End Pagination
 
     const products = await Product.find(find)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip)
+
 
     // console.log(products)
 
+    //render
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword :objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     })
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -32,3 +32,17 @@ if(formSearch) {
     })
 }
 //End Form search
+
+//Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]")
+if(buttonsPagination) {
+    let url = new URL(window.location.href) 
+    buttonsPagination.forEach(button => {
+        button.addEventListener("click", ()=> {
+            const page = button.getAttribute("button-pagination")
+            url.searchParams.set("page", page)
+            window.location.href = url.href
+        })
+    })
+}
+//End Pagination
