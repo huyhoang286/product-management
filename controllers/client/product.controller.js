47db1389203cb2fb1038aec9b194d7ea21932af7
huyhoang286 <huyhoang.m28@gmail.com>
2026-03-26 15:15:38 +0700

    feat(client): cập nhật giao diện trang danh sách và chi tiết sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,24 +2,43 @@
 const Product = require("../../models/product.model")
 
 module.exports.index = async (req, res) => {
+    try {
+        const products = await Product.aggregate([
+            {
+                $match: {
+                    status: "active",
+                    deleted: false
+                }
+            },
+            {
+                $group: {
+                    _id: { $ifNull: ["$styleCode", "$_id"] },
+                    doc: { $first: "$$ROOT" } 
+                }
+            },
+            {
+                $replaceRoot: { newRoot: "$doc" }
+            },
+            {
+                $sort: { createdAt: -1 }
+            }
+        ]);
 
-    const products = await Product.find({
-        status: "active",
-        deleted: false
-    })
-    const newProducts = products.map(item => {
-        item.newPrice = (item.price*(1-item.discountPercentage/100)).toFixed(2)
-        return item
-    })
+        products.forEach(item => {
+            item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
+        });
 
-    // console.log(newProducts)
-
-    res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sản phẩm",
-        products: newProducts
-    })
+        res.render("client/pages/products/index.pug", {
+            pageTitle: "Danh sách sản phẩm",
+            products: products
+        });
+    } catch (error) {
+        console.error(error);
+        res.redirect("/");
+    }
 }
 
+//[GET] /products/:slug
 module.exports.detail = async(req, res) => {
     try {
         const slug = req.params.slug
@@ -32,11 +51,29 @@ module.exports.detail = async(req, res) => {
             return res.redirect("/products")
         }
         product.priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(0)
+
+        let colorways = [];
+        if (product.styleCode) {
+            colorways = await Product.find({
+                styleCode: product.styleCode,
+                status: "active",
+                deleted: false
+            }).select("slug thumbnail color"); 
+        }
+
+        let totalStock = 0;
+        if (product.variants) {
+            totalStock = product.variants.reduce((sum, item) => sum + item.stock, 0);
+        }
+        
         res.render("client/pages/products/detail.pug", {
             pageTitle: product.title,
-            product: product
+            product: product,
+            colorways: colorways,
+            totalStock: totalStock
         })
     } catch(error) {
         res.redirect("/products")
     }
 }
+
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -4,5 +4,4 @@ const controller = require("../../controllers/client/product.controller")
 
 router.get('/', controller.index)
 router.get('/:slug', controller.detail)
-
 module.exports = router;
